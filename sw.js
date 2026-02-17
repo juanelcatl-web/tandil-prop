b69f92291475413f97b27e548e4c80213262cf54
juanelcatl-web <juanelcatl@gmail.com>
2026-02-17 12:02:09 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'tandilprop-v1';
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
